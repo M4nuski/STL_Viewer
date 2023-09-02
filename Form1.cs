77fae253ecff8b8454b6784d802514c2f7f0d93e
Emmanuel Charette <the_man994@hotmail.com>
2023-09-02 01:27:08 -0400

    adding marker on point for mesure
    test point for render panel removal

--- Form1.cs
+++ Form1.cs
@@ -135,7 +135,10 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
 
 
         private Vector3 P1, P2, P3;
+        private Point v1, v2, v3;
         private int MselectedP = 0; // 1 2 3
+        private float[] mmat = new float[16];
+        private Graphics paintG = null;
 
         public Form1()
         {
@@ -202,7 +205,7 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
         private void Form1_Load(object sender, EventArgs e)
         {
             // init stuff
-            WindowInfo = Utilities.CreateWindowsWindowInfo(renderPanel.Handle);
+            WindowInfo = Utilities.CreateWindowsWindowInfo(this.Handle);
             var WindowMode = new GraphicsMode(32, 24, 0, 0, 0, 2);
             WindowContext = new GraphicsContext(WindowMode, WindowInfo, 2, 0, GraphicsContextFlags.Default);
 
@@ -210,7 +213,7 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
             WindowContext.LoadAll();
 
             WindowContext.SwapInterval = 1;
-            GL.Viewport(0, 0, renderPanel.Width, renderPanel.Height);
+            GL.Viewport(0, 0, this.ClientSize.Width, this.ClientSize.Height);
 
             GL.Enable(EnableCap.DepthTest);
 
@@ -385,7 +388,7 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
 
         private void resetSizes()
         {
-            renderPanel.Size = ClientSize;
+           // renderPanel.Size = ClientSize;
             if (WindowInfo != null)
             {
                 setPerspective(_fov, (float)ClientSize.Width / ClientSize.Height, 0.1f, 4096.0f);
@@ -403,6 +406,7 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
         {
             Console.WriteLine("form paint");
             resetSizes();
+            paintG = e.Graphics;
         }
 
         private void ReDraw()
@@ -430,7 +434,7 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
 
             // redraw
             WindowContext.MakeCurrent(WindowInfo);
-            GL.Viewport(0, 0, renderPanel.Width, renderPanel.Height);
+            GL.Viewport(0, 0, this.ClientSize.Width, this.ClientSize.Height);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
                 
             GL.MatrixMode(MatrixMode.Projection);
@@ -503,9 +507,31 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
                     }
 
                 }
-            }   
-
+            }
             WindowContext.SwapBuffers();
+
+            if ((v1.X > 0) && (v1.Y > 0))
+            {
+                panelP1.Visible = true;
+                panelP1.Location = v1;
+            }
+            else panelP1.Visible = false;
+
+            if ((v2.X > 0) && (v2.Y > 0))
+            {
+                panelP2.Visible = true;
+                panelP2.Location = v2;
+            }
+            else panelP2.Visible = false;
+
+            if ((v3.X > 0) && (v3.Y > 0))
+            {
+                panelP3.Visible = true;
+                panelP3.Location = v3;
+            }
+            else panelP3.Visible = false;
+
+
         }
 
         private void setPerspective(float FOV, float AR, float Near, float Far)
@@ -798,6 +824,8 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
             if ((MesurementsPanel.Visible) && (MselectedP > 0))
             {
                 // find Px
+                process_to_viewPort();
+                MselectedP = 0;
                 // update panel data
                 // update 
                 ReDraw();
@@ -1606,8 +1634,121 @@ namespace STLViewer // OpenTK OpenGL 2.0 Immediate mode with pre compiled lists,
             ReDraw();
         }
 
-        private void MesureLabel_Click(object sender, EventArgs e)
+        /*
+                    GL.MatrixMode(MatrixMode.Projection);
+                    GL.LoadMatrix(pmat);
+                    GL.Translate(0.0f, 0.0f, -pz);
+                    GL.Rotate(rx, 1.0f, 0.0f, 0.0f); // ang in degrees
+                    GL.Rotate(ry, 0.0f, 1.0f, 0.0f);
+                    GL.Translate(pivot);
+        */
+
+        private List<Vector3> viewVertices = new List<Vector3>();
+
+        private void process_to_viewPort()
         {
+            if (!UniqueVerticesReady) return;
+
+            //apply transform to matrice
+            mat_copy(ref mmat, ref pmat);
+            mat_translate(ref mmat, 0.0f, 0.0f, -pz);
+            mat_rotateX(ref mmat, rx * 0.0174533f);
+            mat_rotateY(ref mmat, ry * 0.0174533f);
+            mat_translate(ref mmat, pivot.X, pivot.Y, pivot.Z);// TODO recalc only when changing pmat
+
+            viewVertices.Clear(); // TODO dont remake every time
+            //apply matrice to vertex
+            //         public List<Vector3> uniqueVertices
+            // public List<int> uniqueIndices;
+            for (var i = 0; i < uniqueVertices.Count; ++i)
+            {
+                var nv = new Vector3(uniqueVertices[i]);
+                mat_apply(ref mmat, ref nv);
+                viewVertices.Add(nv);
+            }
+           // var nl = new Point();
+            v1.X = (int)(viewVertices[0].X * (ClientSize.Width / 2.0f) + (ClientSize.Width / 2.0f));
+            v1.Y = (int)(-viewVertices[0].Y * (ClientSize.Height / 2.0f) + (ClientSize.Height / 2.0f));
+            v2.X = (int)(viewVertices[1].X * (ClientSize.Width / 2.0f) + (ClientSize.Width / 2.0f));
+            v2.Y = (int)(-viewVertices[1].Y * (ClientSize.Height / 2.0f) + (ClientSize.Height / 2.0f));
+            v3.X = (int)(viewVertices[2].X * (ClientSize.Width / 2.0f) + (ClientSize.Width / 2.0f));
+            v3.Y = (int)(-viewVertices[2].Y * (ClientSize.Height / 2.0f) + (ClientSize.Height / 2.0f));
+            //   labelMark.Location.X = (int)v1.X;
+            // find closest vectex to mouse position and screen
+        }
+
+        private void mat_copy(ref float[] dest, ref float[] source)
+        {
+            for (var i = 0; i < 16; ++i) dest[i] = source[i];
+        }
+        private void mat_translate(ref float[] a, float x, float y, float z)
+        {
+            a[12] = a[0] * x + a[4] * y + a[8]  * z + a[12];
+            a[13] = a[1] * x + a[5] * y + a[9]  * z + a[13];
+            a[14] = a[2] * x + a[6] * y + a[10] * z + a[14];
+            a[15] = a[3] * x + a[7] * y + a[11] * z + a[15];
+        }
+        private void mat_rotateX(ref float[] a, float ang)
+        {
+            float s = (float)Math.Sin(ang);
+            float c = (float)Math.Cos(ang);
+
+            var a04 = a[4];
+            var a05 = a[5];
+            var a06 = a[6];
+            var a07 = a[7];
+            var a08 = a[8];
+            var a09 = a[9];
+            var a10 = a[10];
+            var a11 = a[11];
+
+            a[4] = a04 * c + a08 * s;
+            a[5] = a05 * c + a09 * s;
+            a[6] = a06 * c + a10 * s;
+            a[7] = a07 * c + a11 * s;
+
+            a[8] = a08 * c - a04 * s;
+            a[9] = a09 * c - a05 * s;
+            a[10] = a10 * c - a06 * s;
+            a[11] = a11 * c - a07 * s;
+        }
+        private void mat_rotateY(ref float[] a, float ang)
+        {
+            var s = (float)Math.Sin(ang);
+            var c = (float)Math.Cos(ang);
+
+            var a00 = a[0];
+            var a01 = a[1];
+            var a02 = a[2];
+            var a03 = a[3];
+            var a08 = a[8];
+            var a09 = a[9];
+            var a10 = a[10];
+            var a11 = a[11];
+
+            a[0] = a00 * c - a08 * s;
+            a[1] = a01 * c - a09 * s;
+            a[2] = a02 * c - a10 * s;
+            a[3] = a03 * c - a11 * s;
+
+            a[8] = a00 * s + a08 * c;
+            a[9] = a01 * s + a09 * c;
+            a[10] = a02 * s + a10 * c;
+            a[11] = a03 * s + a11 * c;
+        }
+
+        private void mat_apply(ref float[] m, ref Vector3 a)
+        {
+            var a0 = a[0];
+            var a1 = a[1];
+            var a2 = a[2];
+
+            var w = m[3] * a0 + m[7] * a1 + m[11] * a2 + m[15];
+            if ((w < epsilon) && (w > -epsilon) || (w == float.NaN)) w = 1.0f;
+
+            a[0] = (m[0] * a0 + m[4] * a1 + m[8] * a2 + m[12]) / w;
+            a[1] = (m[1] * a0 + m[5] * a1 + m[9] * a2 + m[13]) / w;
+            a[2] = (m[2] * a0 + m[6] * a1 + m[10] * a2 + m[14]) / w;
 
         }
     }
